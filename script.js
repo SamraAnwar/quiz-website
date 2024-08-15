2f2daaf23b2c28edecf77b44af5e5837a6a7c232
Samra Anwar <anwarahmed0333233@gmail.com>
2024-08-16 00:21:33 +0500

    blur

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// const startBtn = document.querySelector(".start-btn");
-// const popupInfo = document.querySelector(".popup-info");
-// const exitBtn = document.querySelector(".exit-btn");
+const startBtn = document.querySelector(".start-btn");
+const popupInfo = document.querySelector(".popup-info");
+const exitBtn = document.querySelector(".exit-btn");
+const main = document.querySelector(".main");
 
 
-// startBtn.onclick = () => {
-//     popupInfo.classList.add('active');
-// }
-// exitBtn.onclick = () => {
-//     popupInfo.classList.remove('active');
-// }
-function myFunction() {
-  var popup = document.getElementById(".start-btn");
-  popup.classList.toggle("show");
+startBtn.onclick = () => {
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+}
+exitBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
 }
