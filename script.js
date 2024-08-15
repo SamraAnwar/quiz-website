397aea04aafe79cb5fbadae7750d8c55fa59f5e5
Samra Anwar <anwarahmed0333233@gmail.com>
2024-08-15 23:50:49 +0500

    popup

---
+++ script.js
@@ -0,0 +1,15 @@
+// const startBtn = document.querySelector(".start-btn");
+// const popupInfo = document.querySelector(".popup-info");
+// const exitBtn = document.querySelector(".exit-btn");
+
+
+// startBtn.onclick = () => {
+//     popupInfo.classList.add('active');
+// }
+// exitBtn.onclick = () => {
+//     popupInfo.classList.remove('active');
+// }
+function myFunction() {
+  var popup = document.getElementById(".start-btn");
+  popup.classList.toggle("show");
+}
