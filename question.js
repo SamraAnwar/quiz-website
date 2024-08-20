660f8da73bbeb4bc57bbe34472a6308c54c0d6bd
Samra Anwar <anwarahmed0333233@gmail.com>
2024-08-20 15:42:00 +0500

    questions

---
+++ question.js
@@ -0,0 +1,112 @@
+let questions =[
+    {
+        numb: 1,
+        question: "hello spalling?",
+        answer: " hello",
+        options: [
+              "A. bello",
+              "B. hello",
+              "C. gfdhllo",
+              "D. yollo"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for ?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "A. Common Style Sheet",
+            "B. Colorful Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Preprocessor",
+            "C. Hyper Text Programming",
+            "D. Programming Hypertext preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which HTML tag is used to define an internal style sheet?",
+        answer: " <style>",
+        options: [
+            " <style>",
+            " <css>",
+            "<script>",
+            "<link>",
+        ]
+    },
+    {
+        numb: 5,
+        question: "In HTML, which attribute is used to specify that an input field must be filled out",
+        answer: "required",
+        options: [
+            "placeholder",
+            "required",
+            "validate",
+            "mandatory",
+        ]
+    },
+    {
+        numb: 6,
+        question:"Which of the following HTML Elements is used for making any text bold ?",
+        answer:"<b>",
+        options:[
+            "<i>",
+            "<p>",
+            "<b>",
+            "<li>",
+        ]
+    },
+    {
+        numb: 7,
+        question:"How is document type initialized in HTML5.?",
+        answer:"<!DOCTYPE HTML>",
+        options:[
+            "</DOCTYPE HTML>",
+            "</DOCTYPE>",
+            "<!DOCTYPE HTML>",
+            "</DOCTYPE html>",
+        ]
+    },
+    {
+        numb: 8,
+        question:"Which of the following HTML element is used for creating an unordered list?",
+        answer:"<ul>",
+        options:[
+            "<ui>",
+            "<i>",
+            "<em>",
+            "<ul>",
+        ]
+    },
+    {
+        numb: 9,
+        question:"What is the font-size of the h1 heading tag?",
+        answer:"2 em",
+        options:[
+            "3.5 em",
+            "2 em",
+            "2.17 em",
+            "1.5 em",
+        ]
+    },
+    {
+        numb: 10,
+        question:"Which of the following is the correct way of creating an hyperlink in HTML?",
+        answer:"<a href= “www.geeksforgeeks.org”>Geeksforgeeks</a>",
+        options:[
+            "<a>www.geeksforgeeks.org <Geeksforgeeks /a>",
+            "<a href=“www.geeksforgeeks.org” Geeksforgeeks /a>",
+            "<a link=“www.geeksforgeeks.org” Geeksforgeeks> </a>",
+            "<a href= “www.geeksforgeeks.org”>Geeksforgeeks",
+        ]
+    }
+];
