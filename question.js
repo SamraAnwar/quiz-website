5637b0b1ca26657ae941956e210be3774b7d6634
Samra Anwar <anwarahmed0333233@gmail.com>
2024-08-20 15:52:54 +0500

    ques/ans/option

--- question.js
+++ question.js
@@ -1,48 +1,112 @@
-const startBtn = document.querySelector(".start-btn");
-const popupInfo = document.querySelector(".popup-info");
-const exitBtn = document.querySelector(".exit-btn");
-const main = document.querySelector(".main");
-const continueBtn = document.querySelector(".continue-btn");
-const quizSection = document.querySelector(".quiz-section");
-const quizBox = document.querySelector(".quiz-box"); 
-
-startBtn.onclick = () => {
-    popupInfo.classList.add('active');
-    main.classList.add('active');
-}
-exitBtn.onclick = () => {
-    popupInfo.classList.remove('active');
-    main.classList.remove('active');
-}
-continueBtn.onclick = () => {
-    quizSection.classList.add('active');
-    popupInfo.classList.remove('active');
-    main.classList.remove('active');
-    quizBox.classList.add('active');
-
-    showQuestions(0);
-}
-
-let questioncount = 0;
-
-const nextBtn = document.querySelector('.next-btn');
-
-nextBtn.onclick = () => {
-    questioncount++;
-    showQuestions(questioncount);
-}
-
-const optionList = document.querySelector('.option-list');
-// getting ques and option from array
-function showQuestions(index) {
-    const questiontext = document.querySelector('.question-text');
-    questiontext.textContent =`${questions[index].numb}. ${questions[index].question}`;
-
-    let optionTag = `<div class="option"><span>${questions[index].options(0)}</span></div>
-     <div class="option"><span>${questions[index].options(1)}</span></div>
-      <div class="option"><span>${questions[index].options(2)}</span></div>
-       <div class="option"><span>${questions[index].options(3)}</span></div>`;
-
-       optionList.innerHTML = optionTag;
-
-}
+let questions =[
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: " Hyper Text Markup Languag",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multiple Language",
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for ?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "A. Common Style Sheet",
+            "B. Colorful Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Preprocessor",
+            "C. Hyper Text Programming",
+            "D. Programming Hypertext preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which HTML tag is used to define an internal style sheet?",
+        answer: " <style>",
+        options: [
+            " <style>",
+            " <css>",
+            "<script>",
+            "<link>",
+        ]
+    },
+    {
+        numb: 5,
+        question: "In HTML, which attribute is used to specify that an input field must be filled out",
+        answer: "required",
+        options: [
+            "placeholder",
+            "required",
+            "validate",
+            "mandatory",
+        ]
+    },
+    {
+        numb: 6,
+        question:"Which of the following HTML Elements is used for making any text bold ?",
+        answer:"<b>",
+        options:[
+            "<i>",
+            "<p>",
+            "<b>",
+            "<li>",
+        ]
+    },
+    {
+        numb: 7,
+        question:"How is document type initialized in HTML5.?",
+        answer:"<!DOCTYPE HTML>",
+        options:[
+            "</DOCTYPE HTML>",
+            "</DOCTYPE>",
+            "<!DOCTYPE HTML>",
+            "</DOCTYPE html>",
+        ]
+    },
+    {
+        numb: 8,
+        question:"Which of the following HTML element is used for creating an unordered list?",
+        answer:"<ul>",
+        options:[
+            "<ui>",
+            "<i>",
+            "<em>",
+            "<ul>",
+        ]
+    },
+    {
+        numb: 9,
+        question:"What is the font-size of the h1 heading tag?",
+        answer:"2 em",
+        options:[
+            "3.5 em",
+            "2 em",
+            "2.17 em",
+            "1.5 em",
+        ]
+    },
+    {
+        numb: 10,
+        question:"Which of the following is the correct way of creating an hyperlink in HTML?",
+        answer:"<a href= “www.geeksforgeeks.org”>Geeksforgeeks</a>",
+        options:[
+            "<a>www.geeksforgeeks.org <Geeksforgeeks /a>",
+            "<a href=“www.geeksforgeeks.org” Geeksforgeeks /a>",
+            "<a link=“www.geeksforgeeks.org” Geeksforgeeks> </a>",
+            "<a href= “www.geeksforgeeks.org”>Geeksforgeeks",
+        ]
+    }
+];
